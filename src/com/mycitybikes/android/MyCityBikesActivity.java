80163a569d04adb6460f7bfe762ab5ab2bebbc89
jerome.lacoste <jerome.lacoste@gmail.com>
2009-06-22 20:59:51 +0200

    remove unused import

--- src/com/mycitybikes/android/MyCityBikesActivity.java
+++ src/com/mycitybikes/android/MyCityBikesActivity.java
@@ -11,7 +11,6 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.location.LocationProvider;
 import android.os.Bundle;
-import android.os.Debug;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
