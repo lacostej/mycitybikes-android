8a3385eb3a080ff9157576dd31a24d7237ce076f
jerome.lacoste <jerome.lacoste@gmail.com>
2009-06-23 17:49:15 +0200

    cleanup

--- tests/src/com/mycitybikes/android/ClearChannelTest.java
+++ tests/src/com/mycitybikes/android/ClearChannelTest.java
@@ -16,12 +16,6 @@ import com.mycitybikes.android.model.StationLocation;
 
 public class ClearChannelTest extends AndroidTestCase {
 
-	/*
-	 * private final String OSLO_CSV = "#index,lat,lng,id\n" +
-	 * "0,59.92786125852981,10.709009170532226,1\n" +
-	 * "1,59.92805479800621,10.708515644073486,2\n";
-	 */
-
 	private final String OSLO_XML = "<stations>\n"
 			+ "<station><id>01</id><description>Middelthunsgate (vis-a-vis nr. 21, retning Kirkeveien)</description><longitude>10.709009170532226</longitude><latitude>59.92786125852981</latitude></station>\n"
 			+ "<station><id>02</id><description>Middelthunsgate 28 (utenfor Frognerbadet)</description><longitude>10.708515644073486</longitude><latitude>59.92805479800621</latitude></station>\n"
@@ -39,17 +33,6 @@ public class ClearChannelTest extends AndroidTestCase {
 			+ "&lt;description&gt;10-Kirkeristen Øst (mot Storgt.)&lt;/description&gt;"
 			+ "&lt;longitute&gt;10.74714&lt;/longitute&gt;&lt;latitude&gt;59.91313&lt;/latitude&gt;&lt;/station&gt;</string>";
 
-	/*
-	 * public void testLoadOsloBikeLocations() throws IOException {
-	 * List<StationLocation> bikeLocations = new ArrayList<StationLocation>();
-	 * InputStream is = getStringInputStream(OSLO_CSV);
-	 * ClearChannel.loadOsloStationLocations(is, bikeLocations);
-	 * 
-	 * assertEquals("we skip comments and loaded proper locations", 2,
-	 * bikeLocations.size()); assertEquals(1, bikeLocations.get(0).getId());
-	 * assertEquals(59.92786125852981, bikeLocations.get(0).getLatitude());
-	 * assertEquals(10.709009170532226, bikeLocations.get(0).getLongitude()); }
-	 */
 
 	public void testLoadOsloBikeLocations() throws IOException {
 		List<StationLocation> bikeLocations = new ArrayList<StationLocation>();
